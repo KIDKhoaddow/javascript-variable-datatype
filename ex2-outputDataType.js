cf01bcf1aed517057d3d840ad578e6eab4d89494
KIDKhoaddow <phamdangkhoa.k.i.d.6868@gmail.com>
2022-05-09 16:28:30 +0700

    Variable-DataType

---
+++ ex2-outputDataType.js
@@ -0,0 +1,4 @@
+let width = 230
+let height = 10
+let area = width * height
+document.write('Area = ' + area)
---
+++ ex3-outputDataType.js
@@ -0,0 +1,8 @@
+let numb1=parseInt(prompt("Nhập số a"));
+let numb2=parseInt(prompt("Nhập số b"));
+if(numb1%numb2==0){
+    alert("số a là bội số của b");
+}
+else   {
+    alert("số a không là bội số của b");
+}
